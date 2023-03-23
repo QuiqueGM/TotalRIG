a37132fc82ff18bc11ae7b84a7490f6bcde708c8
Quique <quique.grases@gmail.com>
2023-03-23 23:06:16 +0100

    Adding new functions on FillTools script

--- Python/RT_FillTools.py
+++ Python/RT_FillTools.py
@@ -3,12 +3,11 @@ import RT_ErrorsHandler as RTeh
 import RT_Utils
 import maya.cmds as cmds
 
+def autofillFromSelection():
+    RT_Utils.printHeader('AUTOFILL LIMB')
 
-def autofillFromSelection(nameBone):
-    RT_Utils.addObject(nameBone)
+    bones = RT_Utils.createLimbArray(RTvars.armBones if RT_Utils.getHierarchy() == 'Arm' else RTvars.legBones)
     sel = cmds.listRelatives( cmds.ls(sl=True), ad=True )
-    if (RTeh.GetNoSelectionException(sel)): return
-        
     sel.append( cmds.ls(sl=True)[0] )
 
     for s in sel:
@@ -18,7 +17,7 @@ def autofillFromSelection(nameBone):
        
     for s in sel:
         cmds.select(s)
-        for b in RTvars.legBones:
+        for b in bones:
             res = s.find(b)
             if (res > -1):
                 RT_Utils.addObject(b)
@@ -26,5 +25,37 @@ def autofillFromSelection(nameBone):
 
 
 
+def clearAllFields():
+    for b in RTvars.legBones:
+        removeObject(b)
 
 
+
+def removeObject(bone):
+    cmds.textFieldButtonGrp( bone, e=True, tx='' )
+
+
+
+def reloadMirror(bones):
+    replaceBones('L_', 'R_', bones)
+
+
+
+def reloadPosition(bones):
+    replaceBones('F_', 'B_', bones)
+
+
+
+def replaceBones(A, B, bones):
+    for b in range(len(bones)):
+        boneName = cmds.textFieldButtonGrp( bones[b], q=True, tx=True )
+        
+        if (boneName.find(A) > -1):
+            boneName = boneName.replace(A, B)
+        elif (boneName.find(B) > -1):
+            boneName = boneName.replace(B, A)
+            
+        cmds.textFieldButtonGrp(bones[b], e=True, tx=boneName)
+        
+        if b==0:
+            RTvars.limbStartingBone = boneName
