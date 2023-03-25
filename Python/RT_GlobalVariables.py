fadd207dbd8994bccbec5a80b25f522521c6db44
Quique <quique.grases@gmail.com>
2023-03-25 19:03:46 +0100

    Adding new varialbles for hinde leg

--- Python/RT_GlobalVariables.py
+++ Python/RT_GlobalVariables.py
@@ -1,9 +1,9 @@
 version='0.0.1'
 winName = 'AUTORIG'
 
-sides = ['L_', 'R_']
 bone = ''
 ctrlColor = (0, 0, 0)
+limbStartingBone = ''
 chainStartingBone = ''
 
 ClavicleHead = 'ClavicleHead'
@@ -51,29 +51,32 @@ armBones = [ Clavicle, Arm, Forearm, Wrist, Hand ]
 offsetsArm = [ Clavicle, Arm, Forearm, Forearm_PV, Wrist, Wrist_IK, ArmSwitch_FKIK, Hand ]
 bonesHindArm = [ ClavicleHead, Clavicle, Arm, Forearm, Wrist, Hand ]
 offsetsHindArm = [ ClavicleHead, Clavicle, Arm, Arm_PV, Forearm, Forearm_PV, Forearm_IK, Wrist, Wrist_IK, ArmSwitch_FKIK, Hand ]
+armBones = [ Clavicle, Arm, Forearm, Wrist, Hand, FingerRing, FingerMid, FingerIndex, FingerThumb, FingerPinky ]
+offsetsArm = [ Clavicle, Arm, Forearm, Forearm_PV, Wrist, Wrist_IK, ArmSwitch_FKIK, Hand, FingerRing, FingerMid, FingerIndex, FingerThumb, FingerPinky ]
+bonesHindArm = [ ClavicleHead, Clavicle, Arm, Forearm, Wrist, Hand, FingerRing, FingerMid, FingerIndex, FingerThumb, FingerPinky ]
+offsetsHindArm = [ ClavicleHead, Clavicle, Arm, Arm_PV, Forearm, Forearm_PV, Forearm_IK, Wrist, Wrist_IK, ArmSwitch_FKIK, Hand, FingerRing, FingerMid, FingerIndex, FingerThumb, FingerPinky ]
 snapArm = [ Arm, Forearm, Wrist, Wrist_IK ]
 snapHindArm = [ Clavicle, Arm, Forearm, Wrist, Wrist_IK ]
 
-simpleHand = [ FingerThumb, FingerIndex, FingerMid, FingerRing, FingerPinky ]
-hand = [ FingerThumb + phalanges[0], FingerThumb + phalanges[1], FingerThumb + phalanges[2], FingerIndex + phalanges[0], FingerIndex + phalanges[1], FingerIndex + phalanges[2], FingerMid + phalanges[0], FingerMid + phalanges[1], FingerMid + phalanges[2], FingerRing + phalanges[0], FingerRing + phalanges[1], FingerRing + phalanges[2], FingerPinky + phalanges[0], FingerPinky + phalanges[1], FingerPinky + phalanges[2] ]
-
-legBones = [ Hip, UpperLeg, LowerLeg, Ankle, Foot ]
-offsetsLeg = [ Hip, UpperLeg, LowerLeg, LowerLeg_PV, Ankle, Ankle_IK, LegSwitch_FKIK, Foot ]
-bonesHindLeg = [ HipHead, Hip, UpperLeg, LowerLeg, Ankle, Foot ]
-offsetsHindLeg = [ HipHead, Hip, UpperLeg, UpperLeg_PV, LowerLeg, LowerLeg_PV, LowerLeg_IK, Ankle, Ankle_IK, LegSwitch_FKIK, Foot ]
+legBones = [ Hip, UpperLeg, LowerLeg, Ankle, Foot, ToeRing, ToeMid, ToeIndex, ToeThumb, ToePinky ]
+offsetsLeg = [ Hip, UpperLeg, LowerLeg, LowerLeg_PV, Ankle, Ankle_IK, LegSwitch_FKIK, Foot, ToeRing, ToeMid, ToeIndex, ToeThumb, ToePinky ]
+bonesHindLeg = [ HipHead, Hip, UpperLeg, LowerLeg, Ankle, Foot, ToeRing, ToeMid, ToeIndex, ToeThumb, ToePinky ]
+offsetsHindLeg = [ HipHead, Hip, UpperLeg, UpperLeg_PV, LowerLeg, LowerLeg_PV, LowerLeg_IK, Ankle, Ankle_IK, LegSwitch_FKIK, Foot, ToeRing, ToeMid, ToeIndex, ToeThumb, ToePinky ]
 snapLeg = [ UpperLeg, LowerLeg, Ankle, Ankle_IK]
-snapHindLeg = [ Hip, UpperLeg, LowerLeg, Ankle, Ankle_IK]
-
-simpleFoot = [ ToeThumb, ToeIndex, ToeMid, ToeRing, ToePinky ]
-foot = [ ToeThumb + phalanges[0], ToeThumb + phalanges[1], ToeThumb + phalanges[2], ToeIndex + phalanges[0], ToeIndex + phalanges[1], ToeIndex + phalanges[2], ToeMid + phalanges[0], ToeMid + phalanges[1], ToeMid + phalanges[2], ToeRing + phalanges[0], ToeRing + phalanges[1], ToeRing + phalanges[2], ToePinky + phalanges[0], ToePinky + phalanges[1], ToePinky + phalanges[2] ]
+snapHingeLeg = [ Hip, UpperLeg, LowerLeg, Ankle, Ankle_IK]
 
 headBones = ['Head', 'Eye', 'Nose', 'Tongue', 'Jaw', 'Ear', 'Hair', 'Horn', 'Antler', 'Fin']
-bodyBones = ['Spine', 'Chest', 'Neck', 'Head', 'Tail', 'Wing', 'Membrane', 'Claw', 'Ribs', 'Fin']
+bodyBones = ['Spine', 'Chest', 'Neck', 'Tail', 'Wing', 'Membrane', 'Ribs', 'Fin']
+reverseHand = ['HandHeel', 'HandExt', 'HandInt', 'Fingertip', 'HandBall', 'Wrist']
+reverseFoot = ['FootHeel', 'FootExt', 'FootInt', 'Toetip', 'FootBall', 'Ankle']
 centralBones = ['Head', 'Nose', 'Tongue', 'Jaw', 'Root', 'Neck', 'Chest', 'Spine', 'Tail']
 
-
 attributes = ['.translateX', '.translateY', '.translateZ', '.rotateX', '.rotateY', '.rotateZ', '.scaleX', '.scaleY', '.scaleZ']
-transAndRotAttr = ['.translateX', '.translateY', '.translateZ', '.rotateX', '.rotateY', '.rotateZ']
+reverseFootAttr = ['RevFootSeparator', 'FootBalance', 'FootRollWeight', 'FootRoll']
+stretchAttr = ['StretchSeparator', 'StretchCompensation', 'Stretch', 'StretchVolume']
+
+sideGroupArms = [ Arm_PV, Forearm_PV, Forearm_IK, Wrist_IK, ArmSwitch_FKIK, Hand ]
+sideGroupLegs = [ UpperLeg_PV, LowerLeg_PV, LowerLeg_IK, Ankle_IK, LegSwitch_FKIK, Foot ]
 
 CONST = 'CONST'
 PARENT = '__PARENT'
