94cbe0962247fc6035919679ab073aab3eade174
Quique <quique.grases@gmail.com>
2023-05-30 21:34:02 +0200

    Adding Two objects selection exception on errors handler

--- Python/TR_ErrorsHandler.py
+++ Python/TR_ErrorsHandler.py
@@ -1,9 +1,12 @@
+import TR_GlobalVariables as TRvars
+import maya.cmds as cmds
+
 def GetNoSelectionException(sel):
 
     errorSel = False
     
     if not sel:
-        raise RuntimeError("Nothing selected! Please, select an object.")
+        cmds.confirmDialog( t='Selection', m='Nothing selected! Please, select an object.', b=['OK'], p=TRvars.winName )
         errorSel = True
     
     return errorSel
@@ -15,7 +18,19 @@ def GetSelectionException(sel):
     errorSel = GetNoSelectionException(sel)
     
     if len(sel) > 1:
-        raise RuntimeError("More than one object selected! Please, select only a single object.")
+        cmds.confirmDialog( t='Selection', m='More than one object selected! Please, select ONLY a single object.', b=['OK'], p=TRvars.winName )
+        errorSel = True
+    
+    return errorSel
+
+
+
+def GetTwoSelectionException(sel):
+
+    errorSel = False
+    
+    if not len(sel) == 2:
+        cmds.confirmDialog( t='Selection', m='Please, select ONLY two objects.', b=['OK'], p=TRvars.winName )
         errorSel = True
     
     return errorSel
